1572f738f8b4af23e26ec10bed1e04eceb016010
Scstechr <teufelkonig@gmail.com>
2020-01-22 20:51:08 +0900

    mul

--- src/procs.rs
+++ src/procs.rs
@@ -1,5 +1,7 @@
 use crate::pmf::PMF;
 
+const M: i32 = 20;
+
 fn subs(v: &Vec<f64>, x: f64) -> f64 {
     let mut sum = 0.0;
     for (idx, coef) in v.iter().enumerate() {
@@ -10,7 +12,7 @@ fn subs(v: &Vec<f64>, x: f64) -> f64 {
     return sum;
 }
 
-fn print(v: &Vec<f64>) {
+fn print(name: String, v: &Vec<f64>) {
     let mut dist = Vec::new();
     for (idx, coef) in v.iter().enumerate() {
         if coef > &0.0 {
@@ -19,7 +21,7 @@ fn print(v: &Vec<f64>) {
         }
     }
     let dist = dist.join("+");
-    println!("L(x)={}", dist);
+    println!("{}={}", name, dist);
 }
 
 fn deriv(v: &[f64]) -> Vec<f64> {
@@ -32,10 +34,41 @@ fn deriv(v: &[f64]) -> Vec<f64> {
     return ret;
 }
 
+fn binom(n: i32, k: i32) -> i32 {
+    // (c) Knuth
+    (0..n + 1).rev().zip(1..k + 1).fold(1, |mut r, (n, d)| {
+        r *= n;
+        r /= d;
+        r
+    })
+}
+
+fn mul(v: &Vec<f64>, m: &f64) -> Vec<f64> {
+    let mut ret: Vec<f64> = Vec::new();
+    for (idx, coef) in v.iter().enumerate() {
+        ret.push(*coef * m);
+    }
+    return ret;
+}
+
+#[allow(non_snake_case)]
 pub fn run() {
-    let deriv = deriv(&PMF);
-    print(&PMF.to_vec());
-    print(&deriv);
-    println!("sum:{}", subs(&PMF.to_vec(), 1.0));
-    println!("sum:{}", subs(&deriv, 1.0));
+    let Lx = PMF.to_vec();
+    let L1 = subs(&deriv(&Lx), 1.0);
+    let lx = mul(&deriv(&Lx), &(1.0 / L1));
+    print("L(x)".to_string(), &Lx);
+    print("l(x)".to_string(), &lx);
+    let tmax = 0.0;
+    // for i in (0..2) {
+    //     let g = (i as f64) * 0.0001;
+    //     let lmbd = g * L1;
+    //     let mut Rx = Vec::new();
+    //     for k in (0..M) {
+    //         let s = lmbd.powf(k as f64) * (-lmbd).exp() /
+
+    // }
+
+    println!("L(1)={}", subs(&Lx, 1.0));
+    println!("l(1)={}", subs(&lx, 1.0));
+    println!("L'(1)={}", L1);
 }
