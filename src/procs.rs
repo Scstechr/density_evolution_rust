88e32141b7ba8d465864d19bb0473ac0518a1832
Scstechr <teufelkonig@gmail.com>
2020-01-22 20:21:59 +0900

    subs

--- src/procs.rs
+++ src/procs.rs
@@ -1,5 +1,15 @@
 use crate::pmf::PMF;
 
+fn subs(v: &Vec<f64>, x: f64) -> f64 {
+    let mut sum = 0.0;
+    for (idx, coef) in v.iter().enumerate() {
+        if coef > &0.0 {
+            sum += coef * (x).powf(idx as f64);
+        }
+    }
+    return sum;
+}
+
 fn print(v: &Vec<f64>) {
     let mut dist = Vec::new();
     for (idx, coef) in v.iter().enumerate() {
@@ -26,4 +36,6 @@ pub fn run() {
     let deriv = deriv(&PMF);
     print(&PMF.to_vec());
     print(&deriv);
+    println!("sum:{}", subs(&PMF.to_vec(), 1.0));
+    println!("sum:{}", subs(&deriv, 1.0));
 }
